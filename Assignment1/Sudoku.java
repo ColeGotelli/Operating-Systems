267dce1620aae2fb36d64cc61a53eb6ba5c561bd
ColeGotelli <gotel100@mail.chapman.edu>
2019-10-22 20:23:34 -0700

    Progress i think

--- Assignment1/Sudoku.java
+++ Assignment1/Sudoku.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
 import java.util.concurrent.SynchronousQueue;
 
@@ -27,15 +27,20 @@ class Sudoku
 			//Start Thread Here
 			RowChecker row = new RowChecker(grid, data);
 			ColumnChecker col = new ColumnChecker(grid, data);
+			Fixer fixer = new Fixer(grid, data);
 
 			row.start();
 			col.start();
+			fixer.start();
 
-			//Fixer fixer = new Fixer(grid);
-			//fixer.start();
-			//row.join();
-			//col.join();
-			//fixer.join();
+			//System.out.println(row.getState());
+			//System.out.println(col.getState());
+			//System.out.println(fixer.getState());
+
+			row.join();
+			col.join();
+			fixer.join();
+			
 
 		}
 		catch(Exception e) {
@@ -51,6 +56,9 @@ class Data
 	//other thread that takes that data from the queue
 	final SynchronousQueue<int[]> queue = new SynchronousQueue<int[]>();
 	int done = 0;
+	int[] middle;
+	int[] toFix;
+	LinkedList<int[]> fixQueue = new LinkedList<int[]>();
 }
 
 
@@ -79,8 +87,10 @@ class RowChecker extends Thread
 							temp[0] = i;
 							temp[1] = j;
 							temp[2] = k;
-							System.out.println("Found errors at: " + i + ", " + j + " and "+ i + ", " + k);
+							data.middle = temp;
+							System.out.println("Found " +grid[i][j]+ "s at: " + i + ", " + j + " and "+ i + ", " + k);
 							data.queue.put(temp); //Push an array of indexes to the queue
+							Thread.sleep(5);
 						}
 					}
 				}
@@ -90,6 +100,7 @@ class RowChecker extends Thread
 		catch(Exception e) {
 			System.out.println(e);
 		}
+		System.out.println("Made it out Row");
 	}
 }
 
@@ -115,15 +126,30 @@ class ColumnChecker extends Thread
 		try {
 			while(data.done < 9) {
 				int[] temp = data.queue.take(); //Pulls the array of indexes from the queue
-				System.out.println("Taken");
-				for(int j = 0; j < temp.length; ++j) {
-					System.out.println(temp[j]);
+				for(int i = 0; i < 9; ++i) {
+					if(grid[temp[0]][temp[1]] == grid[i][temp[1]]) {
+						System.out.println("I think this pinpoints: " + grid[i][temp[1]]);
+						System.out.println("Found at (" + i + ", " + temp[1] + ")");
+						int[] fixDex = {temp[0],temp[1]};
+						data.toFix = fixDex;
+						data.fixQueue.add(fixDex);
+						Thread.sleep(5);
+					}
+					else if(grid[temp[0]][temp[2]] == grid[i][temp[2]]) {
+						System.out.println("I think this pinpoints: " + grid[i][temp[2]]);
+						System.out.println("Found at (" + i + ", " + temp[2] + ")");
+						int[] fixDex = {temp[0], temp[2]};
+						data.toFix = fixDex;
+						data.fixQueue.add(fixDex);
+						Thread.sleep(5);
+					}
 				}
 			}
 		}
 		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		System.out.println("Made it out Col");
 	}
 }
 
@@ -132,19 +158,174 @@ class ColumnChecker extends Thread
 class Fixer extends Thread 
 {
 	int[][] grid = new int[9][9];
+	private Data data;
 
 	//Create a constructor to pass the array to other threads
-	public Fixer(int[][] table) {
+	public Fixer(int[][] table, Data dt) {
 		grid = table;
+		data = dt;
 	}
 
 	public void run() {
+		fixTable();
+		printTable();
+	}
 
-		//printTable();
+	public void fixTable() {
+		try {
+			Thread.sleep(500);
+			int[] temp = data.toFix;
+			System.out.println("Just grabbed " + temp[0] + ", " + temp[1]);
+			int gridSum = 0;
+			boolean[] subNumbers = new boolean[9];
+			/*SETS IT TO 44 FOR SOME REASON*/
+			if(temp[0] < 3) {
+				gridSum = 0;
+				System.out.println("Do we even try 0?");
+				if (temp[1] < 3) {
+					for(int i = 0; i < 3; ++i) {
+						gridSum += grid[i][i];
+						gridSum += grid[i][i+1];
+						gridSum += grid[i][i+2];
+
+					}
+					int remainder = 45-gridSum;
+					grid[temp[0]][temp[1]] += remainder;
+				}
+				else if (temp[1] >= 3 && temp[1] < 6) {
+
+					subNumbers[grid[3][0]] = true;
+					subNumbers[grid[3][1]] = true;
+					subNumbers[grid[3][2]] = true;
+					subNumbers[grid[4][0]] = true;
+					subNumbers[grid[4][1]] = true;
+					subNumbers[grid[4][2]] = true;
+					subNumbers[grid[5][0]] = true;
+					subNumbers[grid[5][1]] = true;
+					subNumbers[grid[5][2]] = true;
+					for(int i = 0; i < 9; ++i) {
+						if(subNumbers[i] == false) {
+							grid[temp[0]][temp[1]] = i;
+						}
+					}
+					Arrays.fill(subNumbers, Boolean.FALSE);
+					System.out.println("Fixed");
+				}
+				else if (temp[1] > 6) {
+					for(int i = 0; i < 3; ++i) {
+						gridSum += grid[i][i+3];
+						gridSum += grid[i][i+4];
+						gridSum += grid[i][i+5];
+
+						int remainder = 45-gridSum;
+						grid[temp[0]][temp[1]] += remainder;
+					}
+					
+				}
+			}
+			else if(temp[0] >= 3 && temp[0] < 6) {
+				System.out.println("Do we even try 1?");
+				if (temp[1] < 3) {
+					subNumbers[grid[0][3]] = true;
+					subNumbers[grid[0][4]] = true;
+					subNumbers[grid[0][5]] = true;
+					subNumbers[grid[1][3]] = true;
+					subNumbers[grid[1][4]] = true;
+					subNumbers[grid[1][5]] = true;
+					subNumbers[grid[2][3]] = true;
+					subNumbers[grid[2][4]] = true;
+					subNumbers[grid[2][5]] = true;
+					for(int i = 0; i < 9; ++i) {
+						if(subNumbers[i] == false) {
+							grid[temp[0]][temp[1]] = i;
+						}
+					}
+					Arrays.fill(subNumbers, Boolean.FALSE);
+					System.out.println("Fixed");
+				}
+				else if (temp[1] >= 3 && temp[1] < 6) {
+					subNumbers[grid[3][3]] = true;
+					subNumbers[grid[3][4]] = true;
+					subNumbers[grid[3][5]] = true;
+					subNumbers[grid[4][3]] = true;
+					subNumbers[grid[4][4]] = true;
+					subNumbers[grid[4][5]] = true;
+					subNumbers[grid[5][3]] = true;
+					subNumbers[grid[5][4]] = true;
+					subNumbers[grid[5][5]] = true;
+					for(int i = 0; i < 9; ++i) {
+						if(subNumbers[i] == false) {
+							grid[temp[0]][temp[1]] = i;
+						}
+					}
+					Arrays.fill(subNumbers, Boolean.FALSE);
+					System.out.println("Fixed");
+				}
+				else if (temp[1] > 6) {
+					subNumbers[grid[6][3]] = true;
+					subNumbers[grid[6][4]] = true;
+					subNumbers[grid[6][5]] = true;
+					subNumbers[grid[7][3]] = true;
+					subNumbers[grid[7][4]] = true;
+					subNumbers[grid[7][5]] = true;
+					subNumbers[grid[8][3]] = true;
+					subNumbers[grid[8][4]] = true;
+					subNumbers[grid[8][5]] = true;
+					for(int i = 0; i < 9; ++i) {
+						if(subNumbers[i] == false) {
+							grid[temp[0]][temp[1]] = i;
+						}
+					}
+					Arrays.fill(subNumbers, Boolean.FALSE);
+					System.out.println("Fixed");
+				}
+			}
+			else if(temp[0] > 6) {
+				System.out.println("Do we even try 2?");
+				if (temp[1] < 3) {
+					for(int i = 0; i < 3; ++i) {
+						gridSum += grid[6+i][i];
+						gridSum += grid[6+i][i+1];
+						gridSum += grid[6+i][i+2];
+
+					}
+					int remainder = 45-gridSum;
+					grid[temp[0]][temp[1]] += remainder;
+				}
+				else if (temp[1] >= 3 && temp[1] < 6) {
+					for(int i = 0; i < 3; ++i) {
+						gridSum += grid[6+i][i+3];
+						gridSum += grid[6+i][i+4];
+						gridSum += grid[6+i][i+5];
+
+					}
+					int remainder = 45-gridSum;
+					grid[temp[0]][temp[1]] += remainder;
+				}
+				else if (temp[1] > 6) {
+					for(int i = 0; i < 3; ++i) {
+						gridSum += grid[6+i][i+6];
+						gridSum += grid[6+i][i+7];
+						gridSum += grid[6+i][i+8];
+
+					}
+					int remainder = 45-gridSum;
+					grid[temp[0]][temp[1]] += remainder;
+				}
+			}
+			System.out.println("Made it out fixer");
+
+		}
+		catch(Exception e) {}
 	}
 
 	//This code prints out the grid how an actual Sudoku grid looks
 	public void printTable() {
+		try {
+			Thread.sleep(3000);
+		}
+		catch(Exception e) {}
+
 		for(int i = 0; i < 9; ++i) {
 			for(int j = 0; j < 9; ++j) {
 				System.out.print(grid[i][j] + " ");
